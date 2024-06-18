5816e47e9efab23520ab37540ba65672b2512db7
junfengqiao <jfqemma@gmail.com>
2024-06-18 08:31:17 -0400

    created a board and check if it is inside the board

---  knightlOnAChessboard.js
+++  knightlOnAChessboard.js
@@ -5,6 +5,16 @@ function knightlOnAChessboard(n) {
     // loop through the axisX , if increase the x= x +c , if x < n-1 and y +d < n-1; currentPosition is the new X(x=x+c) and new Y(y+d)
     // if x < n-1 and y +d > n-1; x = x+c and y = y-n
     // 
+    // create a result board 
+    let board = new Array(n - 1); 
+    for (let i = 0; i < n - 1; i++) {
+        board[i] = new Array(n - 1).fill(0);
+    }
+
+    // Function to check if a position (x, y) is inside the board
+    function isInBounds(x, y) {
+        return x >= 0 && x < n && y >= 0 && y < n;
+    }
     
 
 }
